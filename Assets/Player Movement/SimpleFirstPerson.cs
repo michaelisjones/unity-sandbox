dbb928cbaaa91b3fa0449da4f77633ab278d06d4
Michael Jones <me@michael.pink>
2017-03-11 19:07:59 +0000

    simple jump script

--- Assets/Player Movement/SimpleFirstPerson.cs
+++ Assets/Player Movement/SimpleFirstPerson.cs
@@ -2,4 +2,35 @@
 
 public class SimpleFirstPerson : MonoBehaviour {
 
+    [Tooltip("Ns")]
+    public float jumpImpulse = 200F;
+    public bool debug = false;
+
+    Vector3 jumpImpulseVector;
+    Rigidbody rb;
+
+    void Awake()
+    {
+        jumpImpulseVector = new Vector3(0F, jumpImpulse, 0F);
+        rb = GetComponent<Rigidbody>();
+
+        if (!rb)
+        {
+            Debug.LogError("no Rigidbody component");
+            enabled = false;
+        }
+    }
+
+    void Update()
+    {
+        if (!enabled) return;
+
+        bool jumpKeyDown = Input.GetKeyDown("space");
+
+        if (jumpKeyDown)
+        {
+            rb.AddForce(jumpImpulseVector, ForceMode.Impulse);
+            if (debug) Debug.Log("applied impulse with vector: " + jumpImpulseVector);
+        }
+    }
 }
