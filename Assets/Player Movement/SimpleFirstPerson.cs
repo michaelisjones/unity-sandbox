8197f44960043c3b2f50fe835d19fff40ad30e3a
Michael Jones <me@michael.pink>
2017-03-12 17:00:28 +0000

    added player movement

--- Assets/Player Movement/SimpleFirstPerson.cs
+++ Assets/Player Movement/SimpleFirstPerson.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 
 public class SimpleFirstPerson : MonoBehaviour
 {
+    [Tooltip("The force applied during movement. /N")]
+    public float movementForce = 2000F;
     [Tooltip("The impulse appliedon the Y axis when jumping. /Ns")]
     public float jumpImpulse = 200F;
     [Tooltip("Minimum time between jumps. /s")]
     public float jumpTimeout = 0.2F;
     [Tooltip("Verbose debug output.")]
-    public bool debug = false;
+    public int debug = 0;
 
     Vector3 jumpImpulseVector;
     Rigidbody rb;
@@ -31,6 +33,13 @@ public class SimpleFirstPerson : MonoBehaviour
     {
         if (!enabled) return;
 
+        if (debug > 1) Debug.Log("Horizontal axis: " + Input.GetAxisRaw("Horizontal") 
+                             + ", Vertical axis: " + Input.GetAxisRaw("Vertical"));
+
+        Vector3 movementForceVector = new Vector3(Input.GetAxisRaw("Horizontal"), 0F, Input.GetAxisRaw("Vertical"));
+        movementForceVector = movementForceVector.normalized * movementForce;
+        rb.AddRelativeForce(movementForceVector);
+
         if (Input.GetButtonDown("Jump"))
         {
             if (Physics.Raycast(transform.position, Vector3.down, 0.2F) 
@@ -38,7 +47,7 @@ public class SimpleFirstPerson : MonoBehaviour
             {
                 lastJumpTime = Time.time;
                 rb.AddForce(jumpImpulseVector, ForceMode.Impulse);
-                if (debug) Debug.Log("applied impulse with vector: " + jumpImpulseVector);
+                if (debug > 0) Debug.Log("applied impulse with vector: " + jumpImpulseVector);
             }
         }
     }
