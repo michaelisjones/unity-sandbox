6f428f29f946cfd575ad2227db4d86eaf9dd13bd
Michael Jones <me@michael.pink>
2017-03-14 17:08:55 +0000

    partial progress

--- Assets/Player Movement/SimpleFirstPerson.cs
+++ Assets/Player Movement/SimpleFirstPerson.cs
@@ -9,6 +9,10 @@ public class SimpleFirstPerson : MonoBehaviour
     public float jumpImpulse = 200F;
     [Tooltip("Minimum time between jumps. /s")]
     public float jumpTimeout = 0.2F;
+    [Tooltip("Maximum gap to the floor to detect touching. /s")]
+    public float floorDetectDistance = 0.1F;
+    [Tooltip("The length of \"button down\" window. Allows pressing jump just before hitting the floor, for example.")]
+    public float earlyInputAllowance = 0.1F;
     [Tooltip("Verbose debug output.")]
     public int debug = 0;
 
@@ -16,6 +20,7 @@ public class SimpleFirstPerson : MonoBehaviour
     Rigidbody rb;
 
     float lastJumpTime = -1F;
+    float lastJumpButtonDownTime = -1F;
 
     void Awake()
     {
@@ -33,16 +38,20 @@ public class SimpleFirstPerson : MonoBehaviour
     {
         if (!enabled) return;
 
-        if (debug > 1) Debug.Log("Horizontal axis: " + Input.GetAxisRaw("Horizontal") 
-                             + ", Vertical axis: " + Input.GetAxisRaw("Vertical"));
+
+        //if (debug > 1) Debug.Log("Horizontal axis: " + Input.GetAxisRaw("Horizontal") 
+        //                     + ", Vertical axis: " + Input.GetAxisRaw("Vertical"));
 
         Vector3 movementForceVector = new Vector3(Input.GetAxisRaw("Horizontal"), 0F, Input.GetAxisRaw("Vertical"));
         movementForceVector = movementForceVector.normalized * movementForce;
         rb.AddRelativeForce(movementForceVector);
 
-        if (Input.GetButtonDown("Jump"))
+
+        if (Input.GetButtonDown("Jump")) lastJumpButtonDownTime = Time.time;
+
+        if (Time.time - lastJumpButtonDownTime < earlyInputAllowance && lastJumpButtonDownTime >= 0)
         {
-            if (Physics.Raycast(transform.position, Vector3.down, 0.2F) 
+            if (Physics.Raycast(transform.position, Vector3.down, floorDetectDistance)
                 && (Time.time - lastJumpTime >= jumpTimeout || lastJumpTime < 0F))
             {
                 lastJumpTime = Time.time;
